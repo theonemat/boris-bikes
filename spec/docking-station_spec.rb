7695bed3f36fc72de3909ab9876169287ddbc94a
saypop <tome.m.dj@gmail.com>
2019-03-06 12:22:08 +0000

    Wrote a test in our spec file and created a release_bike method (see step 8 in readme).

--- spec/docking-station_spec.rb
+++ spec/docking-station_spec.rb
@@ -1,4 +1,11 @@
 require 'docking-station'
-describe "DockingStation" do
-
+describe DockingStation do
+  # write a test that 'expects' DockingStation instances to 'respond_to' the
+  # release_bike method
+  # first attempt
+  # it 'responds to release_bike' do
+  #   expect(subject).to respond_to :release_bike
+  # end
+  # second attempt
+  it { is_expected.to respond_to :release_bike }
 end
