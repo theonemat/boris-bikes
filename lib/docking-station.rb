29c1aa7d79537fb80ded82c0457386ff6e8910b1
saypop <tome.m.dj@gmail.com>
2019-03-07 10:46:29 +0000

    Added test for docking method that accepts a bike then past that test using attr_reader to set an instance variable (step 11).

--- lib/docking-station.rb
+++ lib/docking-station.rb
@@ -1,8 +1,19 @@
 require_relative 'bike'
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  # not required because this is what attr_reader does
+  # def bike
+  #   @bike
+  # end
 
 end
--- spec/docking-station_spec.rb
+++ spec/docking-station_spec.rb
@@ -13,4 +13,13 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+  # write a test for the docking of a bike i.e. a test on a method that requires
+  # an argument
+  # it {is_expected.to respond_to(:dock).with(1).argument }
+  it 'docks a bike' do
+    # bike = subject.release_bike
+    bike = Bike.new  # release_bike no longer creates bike
+    expect(subject.dock(bike)).to eq bike
+  end
+
 end
